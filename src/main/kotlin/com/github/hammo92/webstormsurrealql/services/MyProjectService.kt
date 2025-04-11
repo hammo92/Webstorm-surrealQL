a69bf2de1614bbc0d8561447313c03fae2cf505e
GitHub Action <action@github.com>
2025-04-11 09:23:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/hammo92/webstormsurrealql/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hammo92/webstormsurrealql/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.hammo92.webstormsurrealql.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hammo92.webstormsurrealql.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/hammo92/webstormsurrealql/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hammo92/webstormsurrealql/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hammo92.webstormsurrealql.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
